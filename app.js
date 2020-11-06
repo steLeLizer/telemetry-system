60e3e2260d5f84010caa01fadf5d7158b9ae89fa
Stefan <stefan.djurovic@proventum.me>
2020-11-06 15:41:01 +0100

    writing to Elasticsearch implemented

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const kafka = new Kafka({
     brokers: ['localhost:9092']
 });
 
-// For testing purposes so that it always returns all messages upon rerunning the app
-const groupNamePrefixGenerator = `${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}`;
+// For testing purposes so that it always returns all messages upon rerunning the app to fill the Elasticsearch index data
+// const groupNamePrefixGenerator = `${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}`;
+//
+// const consumer = kafka.consumer({groupId: `${groupNamePrefixGenerator}test-group`});
 
-const consumer = kafka.consumer({groupId: `${groupNamePrefixGenerator}test-group`});
+const consumer = kafka.consumer({groupId: 'telemetry2-group'});
+
+// I assume the Elasticsearch index is created already under the name 'bsocial'
+/*
+PUT bsocial/_doc/1?op_type=create
+{"userData":{"firstName":"Test","lastName":"Test","username":"test.test","email":"test.test@gmail.com"},"registrationDate":"2020-11-04 14:50:53"}
+ */
 
 const consume = async (topic, fromBeginning) => {
     // Consuming
@@ -22,7 +30,20 @@ const consume = async (topic, fromBeginning) => {
     await consumer.run({
         eachMessage: async ({topic, partition, message}) => {
             // console.log(message.value.toString());
-
+            await client.create({
+                index: 'bsocial',
+                type: '_doc',
+                id: message.offset,
+                body: JSON.parse(message.value.toString())
+            }, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`Kafka message ${message.offset} inserted successfully`);
+                    // I am using Kibana to test get _search query
+                    // GET bsocial/_search
+                }
+            });
         }
     })
 };
